399f816232c016ff9efdbb9380f1ee8d69b58c0e
Manvir Singh <Manvirs1611@gmail.com>
2025-05-13 23:34:02 +0530

    FormValidation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,70 @@
+import { useState, useRef } from "react";
+import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 const Login = () => {
-    return <div>Login</div>
-}
+  const [isSignInFrom, setisSignInFrom] = useState(true);
+  const [errormessage, seterrorMessage] = useState(null)
+  const email = useRef(null);
+  const password = useRef(null);
 
-export default Login; 
+  const handleButtonClick = () => {
+    console.log(email.current.value);
+    console.log(password.current.value);
+   const message = checkValidData(email.current.value, password.current.value);
+   seterrorMessage(message)
+  };
+
+  const toggleSignInForm = () => {
+    setisSignInFrom(!isSignInFrom);
+  };
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="logo"
+        />
+      </div>
+      <form onSubmit = {(e) => e.preventDefault()} className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInFrom ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInFrom && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+        <input
+          ref={email}
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+
+        <input
+          ref={password}
+          type="password"
+          placeholder="password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <p className="text-red-500 font-bold text-lg">{errormessage}</p>
+        <button
+          className="p-4 my-6 bg-red-700 w-full rounded-lg"
+          onClick={handleButtonClick}
+        >
+          {isSignInFrom ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInFrom
+            ? " New to Netflix? Sign Up Now"
+            : "Already a User? Sign In Now"}
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
